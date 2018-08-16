95c8811eb61ac11f2411cb3ca2b58989988c52f5
moskaleona <moskaleona@gmail.com>
2018-08-16 20:03:08 +0300

    search for mislabeled basic

--- Voting_filtering/search_for_mislabeled.py
+++ Voting_filtering/search_for_mislabeled.py
@@ -4,6 +4,9 @@ from src.callbacks import LossMetricHistory
 from sklearn.model_selection import train_test_split, StratifiedKFold
 import numpy as np
 from keras.utils import to_categorical
+from keras.models import load_model
+from sklearn.metrics import roc_auc_score
+import os
 
 # Data import and making train, test and validation sets
 sbj = 33
@@ -15,7 +18,7 @@ data = DataBuildClassifier('/home/likan_blk/BCI/NewData/').get_data([sbj],
 X, y = data[sbj][0], data[sbj][1]
 train_ind, test_ind = train_test_split(np.arange(len(y)), shuffle=True,
                                     test_size=0.2, stratify=y,
-                                    random_state=108)
+                                    random_state=8)
 X_train, y_train, X_test, y_test = X[train_ind], y[train_ind], X[test_ind], y[test_ind]
 cv = StratifiedKFold(n_splits=4)
 
@@ -28,18 +31,51 @@ for tr_ind, val_ind in cv.split(X_train, y_train):
     val_inds.append(train_ind[val_ind])# indices of all the validation instances in the initial X array 
 
 # Getting and training models with cross-validation
-epochs=2
+
+time_samples_num = X_train.shape[1]
+channels_num = X_train.shape[2]
+epochs=1
 dropouts = (0.2, 0.4, 0.6)
+logdir = '../logs'
+i = 0
+pure_ind = np.array([])
+bestepochs = np.array([])
 for fold in fold_pairs:
     X_tr, y_tr, X_val, y_val = fold[0], to_categorical(fold[1]), fold[2], to_categorical(fold[3])
-    model, _ = get_model(X_tr.shape[1], X_tr.shape[2], dropouts=dropouts)
-    callback = LossMetricHistory(n_iter=epochs, validation_data=(X_val, y_val), verbose=1)
-    test_history = model.fit(X_tr, y_tr, epochs=epochs,
-                            validation_data=(X_val, y_val), callbacks=[callback],
-                            batch_size=64, shuffle=True)
+    model, _ = get_model(time_samples_num, channels_num, dropouts=dropouts)
+    callback = LossMetricHistory(n_iter=epochs, validation_data=(X_val, y_val),
+                                 verbose=1, fname_bestmodel=os.path.join(logdir,"model%s.hdf5"%(i)))
+    hist = model.fit(X_tr, y_tr, epochs=epochs,
+                    validation_data=(X_val, y_val), callbacks=[callback],
+                    batch_size=64, shuffle=True)
+    bestepochs = np.append(bestepochs, callback.bestepoch)
 
+    # Validation and data cleaning
+    model = load_model(os.path.join(logdir, "model%s.hdf5" % (i)))
+    y_pred = model.predict(X_val)[:,1]
+    # Indices of non-noisy samples
+    pure_ind = np.append(pure_ind,
+                val_inds[i][np.abs(y_val[:,1] - y_pred) < 0.5], axis=0)
+    i += 1
+print(pure_ind)
+bestepoch=round(bestepochs.mean())
 
-# Noise cleaning
-pass
+# Removing instances with noisy labels
+X_train_pure = X[pure_ind]
+y_train_pure = y[pure_ind]
+
 # Testing and comparison of cleaned and noisy data
-pass
+model_noisy, _ = get_model(time_samples_num, channels_num, dropouts=dropouts)
+model_noisy.fit(X_train, y_train, epochs=bestepoch,
+                            batch_size=64, shuffle=True)
+y_pred_noisy = model_noisy.predict(X_test)
+auc_noisy = roc_auc_score(y_test,y_pred_noisy)
+
+model_pure, _ = get_model(time_samples_num, channels_num, dropouts=dropouts)
+model_noisy.fit(X_train, y_train, epochs=bestepoch,
+                            batch_size=64, shuffle=True)
+y_pred_pure = model_pure.predict(X_test)
+auc_pure = roc_auc_score(y_test,y_pred_pure)
+
+print(y_train_pure.shape)
+print (auc_noisy, auc_pure)
