f3aae55803e61b2d603a9f17b31a74bdbabd6dcd
moskaleona <moskaleona@gmail.com>
2019-02-25 19:27:37 +0300

    Added data loading to pipeline

--- LatentSVDD/evaluation.py
+++ LatentSVDD/evaluation.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-def KTA_score(y, gram=None):
+def KTA_score(y, gram):
+    """
+    Computes kernel target alignment score.
+    :param y: target
+    :param gram: Gram matrix
+    :return: float
+    """
     yyT = np.outer(y,y)
     kta = (gram * yyT).sum() / (y.shape[0] * np.linalg.norm(gram))  # <yyT, yyT>F = len(y)
     return kta
+
+if __name__ == '__main__':
+    x = np.array([[1,-2,3],[0,-1,1],[2,1,0]])
+    y = np.array([1,-1, 1])
+    gram = np.dot(x.T, x)
+    print(gram.shape)
+    print(gram)
+    kta = KTA_score(y, gram)
+    print(kta)
--- LatentSVDD/pipeline.py
+++ LatentSVDD/pipeline.py
@@ -1,6 +1,16 @@
-# Data loading
+# Step 0: Data loading and preprocessing
 # Step 1: LatentSVDD
 # Step 2: Outlier removal
 # Step 3: Relabeling
 # Step 4: Evaluation
-# Yeah!
+
+from LatentSVDD.preproc import *
+from LatentSVDD.params import *
+
+
+if __name__=='__main__':
+    data = DataLoader(PATH_TO_DATA).get_data(SUBJECTS, window=(0.2,0.5), baseline_window=(0.2,0.3))
+    for sbj in SUBJECTS:
+        X, y = data[sbj][0], data[sbj][1]
+        features = get_feature_vectors(X)
+        print(features.shape)
